f725f6578a98d1d13aeb22383c8c3fac19439aa7
hambalee <hambalee@outlook.com>
2023-01-10 19:25:52 +0700

    map

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
 func main() {
-	
+	countries := map[string]string{}
+	countries["th"] = "Thailland"
+
+	country, ok := countries["th"]
+	if ok {
+		println(country)
+	} else {
+		println("no value")
+	}
 }
+
