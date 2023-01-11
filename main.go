bd8510313ba34f6d21c8b2cc7bc43c5ffba4b820
hambalee <hambalee@outlook.com>
2023-01-11 20:15:35 +0700

    loop

--- main.go
+++ main.go
@@ -10,5 +10,17 @@ func main() {
 	} else {
 		println("no value")
 	}
-}
 
+	//loop
+	values := []int{10, 20, 30, 40, 50}
+	for i := 0; i < len(values); i++ {
+		println(values[i])
+	}
+	//for range
+	for i, v := range values {
+		println(i, v)
+	}
+	for _, v := range values {
+		println(v)
+	}
+}
